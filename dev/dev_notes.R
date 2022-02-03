d3577fedb64f90bae720ff34e631f86955fb3235
Kyle Harris <koderkow@gmail.com>
2022-02-03 01:33:20 -0600

    versioning and tags

--- dev/dev_notes.R
+++ dev/dev_notes.R
@@ -1,3 +1,9 @@
 attachment::att_amend_desc(
   extra.suggests = c("pkgload", "processx", "rlang")
     )
+
+usethis::use_version()
+
+x <- "v0.1.0"
+gert::git_tag_create(x, x)
+gert::git_tag_push(x)
