509620e44ffde34e0b2ad99adedaa5522e194be5
Kyle Harris <koderkow@gmail.com>
2022-02-03 01:32:39 -0600

    improved word bank

--- R/word_bank.R
+++ R/word_bank.R
@@ -1,6 +1,20 @@
-#' Vector of 5 letter words
+#' Vector of all 5 letter answer words
 #'
-#' Vector that was obtained and filtered from the {words} package.
+#' All wordle words that could be the solution.
 #'
-#' @source \url{https://github.com/cran/words}
+#' @source \url{https://gist.github.com/cfreshman/a03ef2cba789d8cf00c08f767e0fad7b}
+"wordle_answers"
+
+#' Vector of all 5 letter allowed words
+#'
+#' All wordle words that are allowed by wordle.
+#'
+#' @source \url{https://gist.github.com/cfreshman/cdcdf777450c5b5301e439061d29694c}
+"wordle_allowed"
+
+#' Vector of all 5 letter words
+#
+#' Combined two word lists gathered from GitHub user *cfreshman*.
+#'
+#' @source \url{https://gist.github.com/cfreshman}
 "word_bank"
--- data-raw/word_bank.R
+++ data-raw/word_bank.R
@@ -1,7 +1,10 @@
 ## code to prepare `word_bank` dataset goes here
-word_bank <-
-  words::words %>%
-  dplyr::filter(word_length == 5) %>%
-  dplyr::pull(word)
+wordle_answers <- readLines("https://gist.githubusercontent.com/cfreshman/a03ef2cba789d8cf00c08f767e0fad7b/raw/5d752e5f0702da315298a6bb5a771586d6ff445c/wordle-answers-alphabetical.txt")
 
+wordle_allowed <- readLines("https://gist.githubusercontent.com/cfreshman/cdcdf777450c5b5301e439061d29694c/raw/de1df631b45492e0974f7affe266ec36fed736eb/wordle-allowed-guesses.txt")
+
+word_bank <- c(wordle_answers, wordle_allowed)
+
+usethis::use_data(wordle_answers, overwrite = TRUE)
+usethis::use_data(wordle_allowed, overwrite = TRUE)
 usethis::use_data(word_bank, overwrite = TRUE)
